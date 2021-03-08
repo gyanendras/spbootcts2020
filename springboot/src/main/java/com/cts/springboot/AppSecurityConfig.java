b2e676295d408bfd6552cbe9aa6195b8cf9c656e
Gyanendra Singh <gyanendra.singh@congnizant.com>
2021-03-08 13:03:43 +0530

    Added Spring security

---
+++ springboot/src/main/java/com/cts/springboot/AppSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.cts.springboot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig  extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+ 	 //super.configure(auth);
+ 	 auth.inMemoryAuthentication()
+ 	     .withUser("user")
+	     .password(passwordEncoder().encode("userpwd"))
+	     .roles("REG");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
--- springboot/src/test/java/com/cts/springboot/cts2020/test/WeatherSingletonTest.java
+++ springboot/src/test/java/com/cts/springboot/cts2020/test/WeatherSingletonTest.java
@@ -2,12 +2,17 @@ package com.cts.springboot.cts2020.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.Vector;
+
 import org.junit.jupiter.api.Test;
 
 import com.cts.springboot.cts2020.WeatherSingleton;
 
 class WeatherSingletonTest {
-
+ 
 	@Test
 	void testGetWeather() {
 		WeatherSingleton ws = WeatherSingleton.getInstance();
@@ -15,6 +20,9 @@ class WeatherSingletonTest {
 		ws.getWeather("Delhi");
 		ws2.getWeather("Delhi");
 		assertEquals(ws, ws2);
-	}
+		
+}
+	
+	
 
 }
